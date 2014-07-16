dad87f84b7706eb43d7ca67356ddfda7d7fa79b9
LandonWest <landonwest5@gmail.com>
2014-07-15 22:05:56 -0600

    test user validations, remove rspec warnings

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
 
-  describe 'validations', focus do
+  describe 'validations' do
     it { should validate_presence_of :username }
     it { should validate_presence_of :email }
     it { should validate_presence_of :password }
