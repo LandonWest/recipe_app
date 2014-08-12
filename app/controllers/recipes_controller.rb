d43fe6518e7340da4975600ec075249c7266b047
LandonWest <landonwest5@gmail.com>
2014-08-12 08:32:55 -0600

    add belongs_to :user

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,18 @@
 class RecipesController < ApplicationController
-
+  before_action :authenticate_user!, only: [:new, :create]
   def index
     @recipes = Recipe.all
   end
 
   def new
     @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to user_path(current_user)
+    else
+      render :new
   end
 end
