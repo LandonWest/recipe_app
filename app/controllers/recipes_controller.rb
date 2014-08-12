791fdf57ebf28258af0e974f32df7933f4ffa36c
LandonWest <landonwest5@gmail.com>
2014-08-12 08:38:39 -0600

    add create action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,4 +7,12 @@ class RecipesController < ApplicationController
   def new
     @recipe = Recipe.new
   end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to user_path(current_user)
+    else
+      render :new
+  end
 end
