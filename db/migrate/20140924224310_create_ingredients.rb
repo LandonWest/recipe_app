5382ba2de83d2f4847762ce45e7e844794f7121c
LandonWest <landonwest5@gmail.com>
2014-09-24 21:34:41 -0700

    redo ingredients migration

--- db/migrate/20140924224310_create_ingredients.rb
+++ db/migrate/20140924224310_create_ingredients.rb
@@ -2,12 +2,12 @@ class CreateIngredients < ActiveRecord::Migration
   def change
     create_table :ingredients do |t|
       t.belongs_to :recipe
-      t.integer :recipe_id, null:false
-      t.string :name, null:false
-      t.string :quantity, null:false
-      t.string :units, null:false
-
+      t.string :name
+      t.string :quantity
+      t.string :unit_of_measurement
       t.timestamps
     end
+
+    add_index :ingredients, :recipe_id
   end
 end
