9963916e9fee4511e1299d94d5d183d209eda616
LandonWest <landonwest5@gmail.com>
2014-09-24 21:22:51 -0700

    add 'accepts_nested_attributes_for'; add 'after_initialize'; add 'has_blank_attributes' private method

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,11 +1,25 @@
 class Recipe < ActiveRecord::Base
   belongs_to :user
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients, allow_destroy: true, reject_if: :has_blank_attributes
   validates :user_id, :name, :directions, presence: true
 
   mount_uploader :recipe_pic, RecipePicUploader
 
+  after_initialize do
+    if ingredients.empty?
+      ingredients.build
+    end
+  end
+
   def to_param
     [id, name.parameterize].join("-")
+  end
+
+  private
+
+  def has_blank_attributes(ingredient_attrs)
+    ingredient_attrs['quantity'].blank?
   end
 
 end
