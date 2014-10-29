d97abb144fd93ec3a24163bb2e783056a46b2794
LandonWest <landonwest5@gmail.com>
2014-10-28 22:05:22 -0700

    remove redundent picture column from recipes table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140924224310) do
+ActiveRecord::Schema.define(version: 20141029042415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 20140924224310) do
     t.integer  "user_id",     null: false
     t.string   "name",        null: false
     t.text     "description"
-    t.string   "picture"
     t.text     "directions",  null: false
     t.datetime "created_at"
     t.datetime "updated_at"
